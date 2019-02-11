d091ee9c66c9f928c13ad4337b199157660fef01
Wang, Michael <micwang@ea.com>
2019-02-11 00:43:48 -0800

    update - progress at video 13

--- src/posts/NewPost.js
+++ src/posts/NewPost.js
@@ -5,44 +5,15 @@ import {Mutation} from "react-apollo";
 
 class NewPost extends Component {
 
-    state = {
-        title: '',
-        body: '',
-        message: ''
-    }
-
-    handleInput = e => {
-        const formData = {};
-        formData[e.target.name] = e.target.value;
-        formData['message'] = '';
-        this.setState({...formData});
-    }
-
-    handleSubmit = (e, createPost) => {
-        e.preventDefault(); // prevent submit from refreshing page
-        createPost().then(() => {
-            this.setState({title: '', body: '', message: "Successfully Submitted!"});
-        }).catch(e => console.log(e))
-    }
-
     render() {
-
         return (
-            <div>
-                <div className="resultMsg">{this.state.message}</div>
-                <Mutation mutation={CREATE_POST} variables={{...this.state}}>
-                    {createPost => ( // differ from query component, directly referred as simple arrow function, no need to
-                        // pull data out of data comes back
+            <Mutation mutation={CREATE_POST}>
+                {createPost => ( // differ from query component, directly referred as simple arrow function, no need to
+                    // pull data out of data comes back
 
-                        <PostForm
-                            title={this.state.title}
-                            body={this.state.body}
-                            onSubmit={e => this.handleSubmit(e, createPost)}
-                            onChange={this.handleInput}
-                        />
-                    )}
-                </Mutation>
-            </div>
+                    <PostForm onSubmit={createPost}/>
+                )}
+            </Mutation>
         );
     }
 }
--- src/posts/PostForm.js
+++ src/posts/PostForm.js
@@ -1,17 +1,41 @@
 import React, {Component} from 'react';
 
 class PostForm extends Component {
+    state = {
+        title: '',
+        body: '',
+        message: ''
+    }
+
+    handleInput = e => {
+        const formData = {};
+        formData[e.target.name] = e.target.value;
+        formData['message'] = '';
+        this.setState({...formData});
+    }
+
+    handleSubmit = (e, createPost) => {
+        e.preventDefault(); // prevent submit from refreshing page
+        createPost({variables: {...this.state}}).then(() => {
+            this.setState({title: '', body: '', message: "Successfully Submitted!"});
+        }).catch(e => console.log(e))
+    }
 
     render() {
-        const {title, body, onSubmit, onChange} = this.props
+        const {title, body} = this.state
+        const {onSubmit} = this.props;
         return (
-            <form onSubmit={onSubmit}>
-                <label htmlFor='title'>Title</label>
-                <input type='text' name='title' onChange={onChange} placeholder="title" value={title}/>
-                <label htmlFor='body'>Body</label>
-                <textarea type='text' name='body' onChange={onChange} placeholder="body" value={body}/>
-                <button>Submit</button>
-            </form>
+            <div>
+                <div className="resultMsg">{this.state.message}</div>
+                <h1 className="formTitle">New Post Form</h1>
+                <form onSubmit={e => this.handleSubmit(e, onSubmit)}>
+                    <label htmlFor='title'>Title</label>
+                    <input type='text' name='title' onChange={this.handleInput} placeholder="title" value={title}/>
+                    <label htmlFor='body'>Body</label>
+                    <textarea type='text' name='body' onChange={this.handleInput} placeholder="body" value={body}/>
+                    <button>Submit</button>
+                </form>
+            </div>
         );
     }
 }
